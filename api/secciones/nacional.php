229f4a61a94cd8f9c4ca6ab74013ec11e113994b
Manu0809fm <manuelvirgo0809@hotmail.com>
2025-10-28 16:57:31 -0500

    Add files via upload

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $nacional = [
 "titulo" => "Lucinda Vásquez se pronuncia tras ser captada cuando su asesor le cortaba las uñas",
 "autor" => "Jair Zevallos Morón",
-"resumen" => "La congresista rechazó las críticas en su contra y sostuvo que las imágenes difundidas responden a una "venganza" de extrabajadores de su despacho. En tanto, la Comisión de Ética evalúa abrirle una investigación por presunto uso indebido de recursos del Estado.",
+"resumen" => "La congresista rechazó las críticas en su contra y sostuvo que las imágenes difundidas responden a una venganza de extrabajadores de su despacho. En tanto, la Comisión de Ética evalúa abrirle una investigación por presunto uso indebido de recursos del Estado.",
 ];
 ?>
